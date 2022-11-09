a692fa9bdaa295fc51aca7cb7c3b703516acab9f
Charles Feval <fevalc>
2022-11-08 17:02:11 -0800

    PP image url fix

--- src/PrivacyPage.tsx
+++ src/PrivacyPage.tsx
@@ -23,7 +23,7 @@ export function PrivacyPage() {
   return <Container>
     <h1 style={styles.title}>Moostik CRM</h1>
     <h2>Privacy Policy</h2>
-    <img src="/img/moostik.png" style={styles.logo}></img> 
+    <img src="../img/moostik.png" style={styles.logo}></img> 
     <p>Moostik CRM is collecting and storing all information you provide on that website for analytics purposes, without limitation of duration for its storage.</p>
     <p>We respect your privacy. If you live in European Union you have a right to submit a request to retrieve or modify the data that we store on you. Please submit a request at privacy _at_ cingen.net, with a proof of ID, and we will process it within 30 days.</p>
   </Container>
